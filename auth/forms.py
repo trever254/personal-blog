39d3c6d7bc3ee00b3ff10e02368c7f357441a55f
trever <toakumu@gmail.com>
2020-07-20 16:13:02 +0300

    added class login form

--- auth/forms.py
+++ auth/forms.py
@@ -10,3 +10,17 @@ class RegistrationForm(FlaskForm):
     password = PasswordField('password',validators = [Required(),EqualTo('password_confirm',message = 'Password does not match')])
     password_confirm  = PasswordField('Cornfirm password',validators = [Required()])
     submit = SubmitField('Sign Up')
+
+    def validate_email(self,data_field):
+        if User.query.filter_by(email = data_field.data).first():
+            raise ValidationError('There is an account with that email')
+
+    def validate_username(self,data_field):
+        if User.query.filter_by(username = data_field.data).first():
+            raise ValidationError('That username is taken')
+
+class LoginForm(FlaskForm):
+    email = StringField('Your Email Address',validators=[Required(),Email()])  
+    password = PasswordField('password',validators =[Required()])
+    remember = BooleanField('Remember me')
+    submit = SubmitField('Sign In')
