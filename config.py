b60139bd6448ba6ae06bf09b94cac172e7fbedcc
trever <toakumu@gmail.com>
2020-07-20 14:15:17 +0300

    Added class dev and pod in config.py

--- config.py
+++ config.py
@@ -16,3 +16,20 @@ class Config:
     MAIL_USE_TLS = True
     MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+
+    @staticmethod
+    def init_app(app):
+        pass
+
+class ProdConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    DEBUG = True
+
+class DevConfig(Config):
+    SQLALCHEMY_DATABASE_URI = ''
+    pass
+
+config_options = {
+    'development':DevConfig,
+    'production':ProdConfig
+}
