9de37e40b872e3a25eb12c31c91883d130ee2fd1
trever <toakumu@gmail.com>
2020-07-20 10:21:08 +0300

    added file config.py

---
+++ config.py
@@ -0,0 +1,18 @@
+import os
+
+class Config:
+    QUOTE_BASE_URL = 'http://quotes.stormconsultancy.co.uk/random.json'
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    # simple mde configurations
+    SIMPLEMDE_JS_IIFE = True
+    SIMPLEMDE_USE_CDN = True
+
+    # email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
